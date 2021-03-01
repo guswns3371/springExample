6f9dd22181184d5bf218af11ab11af4df5a953e9
hjoon <guswns3371@gmail.com>
2021-03-01 17:08:30 +0900

    [ADD] add view posts-save.mustache

--- src/main/java/com/example/springExample/web/IndexController.java
+++ src/main/java/com/example/springExample/web/IndexController.java
@@ -8,6 +8,15 @@ public class IndexController {
 
     @GetMapping("/")
     public String index() {
+        // "/"를 호출하면, index.mustache 가 호출된다.
         return "index";
+    }
+
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+
+        // "/posts/save" 를 호출하면, posts-save.mustache 를 호출한다.
+        return "posts-save";
     }
 }
